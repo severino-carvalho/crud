f3d7a5c86af4aaf06003f0869174cc66d28590bd
oh-netin <severinocarvalho14@gmail.com>
2021-09-28 13:33:46 -0300

    Atualização das operação para funcionarem com o Banco de Dados

--- src/main/java/br/edu/ifrn/crud/controllers/BuscaUsuarioController.java
+++ src/main/java/br/edu/ifrn/crud/controllers/BuscaUsuarioController.java
@@ -1,14 +1,9 @@
-/**
- * 
- */
+
 package br.edu.ifrn.crud.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpSession;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,35 +13,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifrn.crud.domains.Usuario;
+import br.edu.ifrn.crud.repository.UsuarioRepository;
 
 @Controller
 @RequestMapping("/usuarios")
 public class BuscaUsuarioController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@GetMapping("/busca")
 	public String entrarBusca() {
 		return "/usuario/busca";
 	}
 
-	@SuppressWarnings("unchecked")
 	@GetMapping("/buscar")
 	public String buscar(@RequestParam(name = "nome", required = false) String nome,
-			@RequestParam(name = "mostrarTodosDados", required = false) Boolean mostrarTodosDados, HttpSession sessao,
-			ModelMap model) {
+			@RequestParam(name = "nome", required = false) String email,
+			@RequestParam(name = "mostrarTodosDados", required = false) Boolean mostrarTodosDados, ModelMap model) {
 
-		List<Usuario> usuariosCadastrados = (List<Usuario>) sessao.getAttribute("usuariosCadastrados");
-
-		List<Usuario> usuariosEncontrados = new ArrayList<>();
-
-		if (nome == null || nome.isEmpty()) {
-			usuariosEncontrados = usuariosCadastrados;
-		} else {
-			if (usuariosCadastrados != null) {
-				usuariosEncontrados = usuariosCadastrados.stream()
-						.filter(u -> u.getNome().toLowerCase().contains(nome.toLowerCase()))
-						.collect(Collectors.toList());
-			}
-		}
+		List<Usuario> usuariosEncontrados = usuarioRepository.findByEmailAndNome(email, nome);
 
 		model.addAttribute("usuariosEncontrados", usuariosEncontrados);
 
@@ -57,22 +43,11 @@ public class BuscaUsuarioController {
 		return "/usuario/busca";
 	}
 
-	@SuppressWarnings({ "unchecked" })
 	@GetMapping("/remover/{id}")
-	public String remover(@PathVariable("id") Integer idUsuario, HttpSession sessao, RedirectAttributes attr) {
+	public String remover(@PathVariable("id") Integer idUsuario, RedirectAttributes attr) {
 
-		List<Usuario> usuariosCadastrados = (List<Usuario>) sessao.getAttribute("usuariosCadastrados");
-
-		Usuario u = new Usuario();
-		u.setId(idUsuario);
-
-		boolean removeu = usuariosCadastrados.remove(u);
-
-		if (removeu) {
-			attr.addFlashAttribute("msgSucesso", "Usuário removido com sucesso!");
-		} else {
-			attr.addFlashAttribute("msgErro", "Não foi possível remover o usuário!");
-		}
+		usuarioRepository.deleteById(idUsuario);
+		attr.addFlashAttribute("msgSucesso", "Usuário removido com sucesso!");
 
 		return "redirect:/usuarios/buscar";
 	}
--- src/main/java/br/edu/ifrn/crud/controllers/CadastroUserController.java
+++ src/main/java/br/edu/ifrn/crud/controllers/CadastroUserController.java
@@ -1,12 +1,11 @@
 package br.edu.ifrn.crud.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -18,10 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifrn.crud.domains.Usuario;
+import br.edu.ifrn.crud.repository.UsuarioRepository;
 
 @Controller
 @RequestMapping("/usuarios")
 public class CadastroUserController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@GetMapping("/cadastro")
 	public String entrarCadastro(ModelMap modelUser) {
@@ -34,110 +37,37 @@ public class CadastroUserController {
 		return Arrays.asList("Argricultor", "Engenheiro", "Médico", "Professor", "Outro");
 	}
 
-	@SuppressWarnings("unchecked")
 	@PostMapping("/salvar")
-	public String salvarUser(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr, HttpSession sessao) {
+	public String salvarUser(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr, ModelMap modelo) {
 
-		// Execulta o método de validação e se houver erros coloca o seu retorno dentro da variável validação
-		List<String> validacao = validacao(usuario);
-		
-		// Se houver erro então vai ser mandado para a página.
-		if (!validacao.isEmpty()) {
-			attr.addFlashAttribute("msgErro", validacao);
-			return "redirect:/usuarios/cadastro";
-		}
-		
-		// Se houver erros no objeto usuário preenchido ele vai ser capturado no IF e não vai ser realizado o cadastro
+		// Se houver erros no objeto usuário preenchido ele vai ser capturado no IF e
+		// não vai ser realizado o cadastro
 		if (result.hasErrors()) {
 			// É retornada para a mesma página para os erros serem mostrados
-			return "/usuario/cadastro"; 
-		}
-		
-		Integer id = (Integer) sessao.getAttribute("id");
-		List<Usuario> usuariosCadastrados = (List<Usuario>) sessao.getAttribute("usuariosCadastrados");
-
-		if (id == null) {
-			id = 1;
+			return "/usuario/cadastro";
 		}
 
-		if (usuariosCadastrados == null) {
-			usuariosCadastrados = new ArrayList<>();
-		}
+		/**
+		 * IMPLEMENTAÇÃO DO CADASTRO DE USUÁRIOS NO BANCO DE DADOS
+		 */
 
-		if (usuario.getId() == 0) {
-
-			usuario.setId(id);
-			usuariosCadastrados.add(usuario);
-
-			id++;
-
-			sessao.setAttribute("id", id);
-			sessao.setAttribute("usuariosCadastrados", usuariosCadastrados);
-
-			attr.addFlashAttribute("msgSucesso", "Cadastro de usuário realizado com sucesso!");
-
-		} else {
-
-			usuariosCadastrados.remove(usuario);
-			usuariosCadastrados.add(usuario);
-
-			attr.addFlashAttribute("msgSucesso", "Edição de usuário realizada com sucesso!");
-
+		try {
+			usuarioRepository.save(usuario);
+		} catch (Exception e) {
+			modelo.addAttribute("msgErro", "ERRO INTERNO NO SERVIDOR");
+			return "usuario/cadastro";
 		}
 
 		return "redirect:/usuarios/cadastro";
 	}
 
-	@SuppressWarnings("unchecked")
 	@GetMapping("/editar/{id}")
-	public String iniciarEdicao(@PathVariable("id") Integer idUser, ModelMap model, HttpSession sessao) {
+	public String iniciarEdicao(@PathVariable("id") Integer idUser, ModelMap model) {
 
-		List<Usuario> usuariosCadastrados = (List<Usuario>) sessao.getAttribute("usuariosCadastrados");
+		Usuario u = usuarioRepository.findById(idUser).get();
 
-		Usuario usuario = new Usuario();
-		usuario.setId(idUser);
-
-		int pos = usuariosCadastrados.indexOf(usuario);
-		usuario = usuariosCadastrados.get(pos);
-
-		model.addAttribute("usuario", usuario);
+		model.addAttribute("usuario", u);
 
 		return "/usuario/cadastro";
-	}
-	
-	public List<String> validacao(Usuario u) {
-		List<String> validation = new ArrayList<>();
-		
-		if (u.getNome() == null || u.getNome().isEmpty()) {
-			validation.add("Campo 'Nome' é obrigatório.");
-		}
-		if (u.getNome().length() < 3) {
-			validation.add("O campo 'Nome' deve conter ao menos 3 caracteres.");
-		}
-		if (u.getEmail() == null || u.getEmail().isEmpty()) {
-			validation.add("Campo 'Email' é obrigatório.");
-		}
-		if (u.getTelefone() == null || u.getTelefone().isEmpty()) {
-			validation.add("Campo 'Telefone' é obrigatório.");
-		}
-		if (u.getTelefone().length() != 9) {
-			validation.add("O campo 'Telefone' deve conter somente 9 dígitos.");
-		}
-		if (u.getSenha() == null || u.getSenha().isEmpty()) {
-			validation.add("Campo 'Senha' é obrigatório.");
-		}
-		if (u.getSenha().length() < 6 || u.getSenha().length() > 15) {
-			validation.add("A senha deve conter dentre 6 a 15 caractéres");
-		}
-		
-		if (u.getSexo() == null || u.getSexo().isEmpty()) {
-			validation.add("Campo 'Sexo' é obrigatório.");
-		}
-		
-		if (u.getProfissao() == null || u.getProfissao().isEmpty()) {
-			validation.add("Campo 'Profissao' é obrigatório.");
-		}
-		
-		return validation;
 	}
 }
