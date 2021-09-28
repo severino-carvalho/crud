52d8df498ba9e20c7816b77b2119d6e029133f34
oh-netin <severinocarvalho14@gmail.com>
2021-09-28 13:32:22 -0300

    Criação do repositório de usuário

---
+++ src/main/java/br/edu/ifrn/crud/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package br.edu.ifrn.crud.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.edu.ifrn.crud.domains.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    @Query("select u from Usuario u where u.email like %:email% and u.nome like %:nome%")
+    List<Usuario> findByEmailAndNome(@Param("email") String email, @Param("nome") String nome);
+
+}
