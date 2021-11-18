75ca17da2792fed68169c93ecb7ecc1f778121c8
severino-carvalho <severinocarvalho14@gmail.com>
2021-11-18 10:07:51 -0300

    Alteraçao no nome da classe

---
+++ src/main/java/br/edu/ifrn/crud/dto/AutocompleteDTO.java
@@ -0,0 +1,25 @@
+package br.edu.ifrn.crud.dto;
+
+public class AutocompleteDTO {
+
+    private String label;
+
+    private Integer value;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+}
