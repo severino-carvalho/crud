98c274c7cb2e999defc05209330a435478428386
severino-carvalho <severinocarvalho14@gmail.com>
2021-11-18 17:52:27 -0300

    Atualizaçao do nome da classe e implementaçao da mesma

--- src/main/java/br/edu/ifrn/crud/dto/AutocompleteDTO.java
+++ src/main/java/br/edu/ifrn/crud/dto/AutocompleteDTO.java
@@ -6,6 +6,11 @@ public class AutocompleteDTO {
 
     private Integer value;
 
+    public AutocompleteDTO(String label, Integer value) {
+        this.label = label;
+        this.value = value;
+    }
+
     public String getLabel() {
         return label;
     }
