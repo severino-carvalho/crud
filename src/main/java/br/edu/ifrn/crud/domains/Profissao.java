f883a973a29ec1da4add15689cdc5df56ca0ce5b
severino-carvalho <severinocarvalho14@gmail.com>
2021-11-18 08:58:32 -0300

    Criaçao e implementaçao da classe Profissao

---
+++ src/main/java/br/edu/ifrn/crud/domains/Profissao.java
@@ -0,0 +1,18 @@
+package br.edu.ifrn.crud.domains;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Profissao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+}
