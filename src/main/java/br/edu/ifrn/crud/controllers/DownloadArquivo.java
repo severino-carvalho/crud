dfaf3bddf32e2bc62850f19303cd76fd31c73374
severino-carvalho <severinocarvalho14@gmail.com>
2021-12-01 18:57:19 -0300

    Implementacao da classe de Download

--- src/main/java/br/edu/ifrn/crud/controllers/DownloadArquivo.java
+++ src/main/java/br/edu/ifrn/crud/controllers/DownloadArquivo.java
@@ -1,9 +1,17 @@
 package br.edu.ifrn.crud.controllers;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import br.edu.ifrn.crud.domains.Arquivo;
 import br.edu.ifrn.crud.repository.ArquivoRepository;
 
 @Controller
@@ -12,7 +20,15 @@ public class DownloadArquivo {
     private ArquivoRepository arquivoRepository;
 
     @GetMapping("/download/{idArquivo}")
-    public String download() {
-        return "";
+    public ResponseEntity<?> downloadFile(@PathVariable Long idArquivo, @PathParam("salvar") String salvar) {
+
+        Arquivo file = arquivoRepository.findById(idArquivo).get();
+
+        String texto = (salvar == null || salvar.equals("true"))
+                ? "attachment; filename=\"" + file.getNomeArquivo() + "\""
+                : "inline; filename=\"" + file.getNomeArquivo() + "\"";
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(file.getTipoArquivo()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, texto).body(new ByteArrayResource(file.getDados()));
     }
 }
