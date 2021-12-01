c1c1e647570b9ed4836eabf7d7232b40318281bb
severino-carvalho <severinocarvalho14@gmail.com>
2021-12-01 14:42:14 -0300

    Criacao da entidade Arquivo

---
+++ src/main/java/br/edu/ifrn/crud/domains/Arquivo.java
@@ -0,0 +1,90 @@
+package br.edu.ifrn.crud.domains;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Arquivo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeArquivo;
+
+    private String tipoArquivo;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] dados;
+
+    public Arquivo(String nomeArquivo, String tipoArquivo, byte[] dados) {
+        super();
+        this.nomeArquivo = nomeArquivo;
+        this.tipoArquivo = tipoArquivo;
+        this.dados = dados;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeArquivo() {
+        return nomeArquivo;
+    }
+
+    public void setNomeArquivo(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public String getTipoArquivo() {
+        return tipoArquivo;
+    }
+
+    public void setTipoArquivo(String tipoArquivo) {
+        this.tipoArquivo = tipoArquivo;
+    }
+
+    public byte[] getDados() {
+        return dados;
+    }
+
+    public void setDados(byte[] dados) {
+        this.dados = dados;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Arquivo other = (Arquivo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
