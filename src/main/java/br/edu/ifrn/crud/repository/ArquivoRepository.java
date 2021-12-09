27647a6da70874e3c4bc070eb827acdd77b32a3c
severino-carvalho <severinocarvalho14@gmail.com>
2021-12-09 12:17:51 -0300

    Atualizacao no HQL

--- src/main/java/br/edu/ifrn/crud/repository/ArquivoRepository.java
+++ src/main/java/br/edu/ifrn/crud/repository/ArquivoRepository.java
@@ -1,9 +1,16 @@
 package br.edu.ifrn.crud.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.edu.ifrn.crud.domains.Arquivo;
 
 public interface ArquivoRepository extends JpaRepository<Arquivo, Long> {
-    
+
+    @Query("select a from Arquivo a where a.nomeArquivo like %:nome%")
+    List<Arquivo> findByNome(@Param("nome") String nome);
+
 }
--- src/main/java/br/edu/ifrn/crud/repository/UsuarioRepository.java
+++ src/main/java/br/edu/ifrn/crud/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ifrn.crud.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,9 @@ import org.springframework.data.repository.query.Param;
 import br.edu.ifrn.crud.domains.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    @Query("select u from Usuario u where u.email like %:email%")
+    Optional<Usuario> findByEmail(@Param("email") String email);
 
     @Query("select u from Usuario u where u.email like %:email% and u.nome like %:nome%")
     List<Usuario> findByEmailAndNome(@Param("email") String email, @Param("nome") String nome);
